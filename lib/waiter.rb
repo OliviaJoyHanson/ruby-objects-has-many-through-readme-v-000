2e149f12fc37af4961858e6a7c585e60d0058cba
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-09-21 17:24:58 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
+
+  def most_freq_customer
+    most_freq_customer_meal = meals.max_by{|meal| meals.count(meal)}
+    most_freq_customer_meal.customer
+  end
+
+  def meal_of_worst_tipper
+    meal_of_worst_tipper = meals.min do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  end
+
 
 end
